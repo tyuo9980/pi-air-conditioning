95c1f517921b761577b6eda942af0947ca7d2d13
Peter Li <tyuo9980@gmail.com>
2026-08-15 20:18:06 -0400

    servo

--- servo.py
+++ servo.py
@@ -5,12 +5,16 @@ from gpiozero import AngularServo
 # Setup servo on GPIO 18 with standard pulse widths
 servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0024)
 
-servo.angle = 0
-
 def current_position() -> float:
-    """Return the servo's last commanded angle in degrees."""
     return servo.angle
 
 def move_to(angle: float) -> float:
     servo.angle = angle
+    sleep(2)
+    print(servo.angle)
+    return servo.angle
     
+
+#move_to(-90)
+#move_to(90)
+
