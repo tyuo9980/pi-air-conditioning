7343016ad01df94942c86e3dc53ed7a3acc8019f
Peter Li <tyuo9980@gmail.com>
2026-08-16 00:03:12 -0400

    servo calibration

--- servo.py
+++ servo.py
@@ -1,9 +1,18 @@
 from time import sleep
 
 from gpiozero import AngularServo
+from gpiozero.pins.pigpio import PiGPIOFactory
 
-# Setup servo on GPIO 18 with standard pulse widths
-servo = AngularServo(18, min_pulse_width=0.00055, max_pulse_width=0.0024)
+factory = PiGPIOFactory()
+
+servo = AngularServo(
+    18,
+    min_angle=-90,
+    max_angle=90,
+    min_pulse_width=0.0005,
+    max_pulse_width=0.0025,
+    pin_factory=factory
+)
 
 def current_position() -> float:
     return servo.angle
@@ -20,4 +29,3 @@ def sweep():
         move_to(angle)
         angle += 5
 
-        
